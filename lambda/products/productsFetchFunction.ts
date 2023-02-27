3cba20fa622c765922033083284bc8c0cebb029a
Filipe <filipe.viana@ufv.br>
2023-02-27 12:35:15 -0300

    chore: add scopre to get product by id

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,24 +1,30 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    
-    const { resource, httpMethod, requestContext } = event;
+	const { resource, httpMethod, requestContext, pathParameters } = event;
 	const { awsRequestId } = context;
-    const { requestId: apiRequestId } = requestContext;
+	const { requestId: apiRequestId } = requestContext;
 
-    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${awsRequestId}`)
-
+	console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${awsRequestId}`);
 	if (resource === "/products") {
-		if (httpMethod === "GET") {
-			console.log("GET");
+		console.log("GET products");
 
-			return {
-				statusCode: 200,
-				body: JSON.stringify({
-                    message: "Get Products - OK",
-                }),
-			};
-		}
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: "Get Products - OK",
+			}),
+		};
+	} else if (resource === "/products/{id}") {
+		const productId = pathParameters!.id as string;
+		console.log(`GET product by id: ${productId} `);
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				message: `GET product by id: ${productId} `,
+			}),
+		};
 	}
 
 	return {
