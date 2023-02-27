fca64a53dd89c74243deaeff0c76ce9d8b88abbf
Filipe <filipe.viana@ufv.br>
2023-02-27 12:24:22 -0300

    chore: add new paths to api gateway

--- lib/ecommerceAPI-stack.ts
+++ lib/ecommerceAPI-stack.ts
@@ -1,41 +1,49 @@
 import * as cdk from "aws-cdk-lib";
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwlogs from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 interface EcommerceApiStackProps extends cdk.StackProps {
-	productsFetchHandler: lambdaNodeJS.NodejsFunction;
+	productsFetchHandler: NodejsFunction;
+	productsAdminHandler: NodejsFunction;
 }
 
 export class EcommerceApiStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
 		super(scope, id, props);
 
-        const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs")
+		const logGroup = new cwlogs.LogGroup(this, "ECommerceApiLogs");
 
 		const api = new apigateway.RestApi(this, "ECommerceApi", {
 			restApiName: "ECommerceRestApi",
-            cloudWatchRole: true,
-            deployOptions: {
-                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
-                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
-                    httpMethod: true,
-                    ip: false,
-                    protocol: true,
-                    requestTime: true,
-                    resourcePath: true,
-                    responseLength: true,
-                    status: true,
-                    caller: true,
-                    user: false
-                })
-            }
+			cloudWatchRole: true,
+			deployOptions: {
+				accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+				accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+					httpMethod: true,
+					ip: false,
+					protocol: true,
+					requestTime: true,
+					resourcePath: true,
+					responseLength: true,
+					status: true,
+					caller: true,
+					user: false,
+				}),
+			},
 		});
 
 		const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler);
+		const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler);
 
 		const productsResource = api.root.addResource("products");
+		const productsIdResource = productsResource.addResource("{id}");
+
 		productsResource.addMethod("GET", productsFetchIntegration);
+		productsResource.addMethod("POST", productsAdminIntegration);
+		productsIdResource.addMethod("GET", productsAdminIntegration);
+		productsIdResource.addMethod("PUT", productsAdminIntegration);
+		productsIdResource.addMethod("DELETE", productsAdminIntegration);
 	}
 }
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,23 +1,61 @@
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
-import * as cdk from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Stack, StackProps, Duration, RemovalPolicy } from "aws-cdk-lib";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
 
-export class ProductsAppStack extends cdk.Stack {
-	readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
-	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ProductsAppStack extends Stack {
+	readonly productsFetchHandler: NodejsFunction;
+	readonly productsDdb: Table;
+	readonly productsAdminHandler: NodejsFunction;
+
+	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
-		this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
+
+		this.productsDdb = new Table(this, "ProductsDdb", {
+			tableName: "products",
+			removalPolicy: RemovalPolicy.DESTROY,
+			partitionKey: {
+				name: "id",
+				type: AttributeType.STRING,
+			},
+			billingMode: BillingMode.PROVISIONED,
+			readCapacity: 1,
+			writeCapacity: 1,
+		});
+
+		this.productsFetchHandler = new NodejsFunction(this, "ProductsFetchFunction", {
 			entry: "lambda/products/productsFetchFunction.ts",
 			functionName: "ProductsFetchFunction",
-            handler: "handler",
-            memorySize: 128,
-            timeout: cdk.Duration.seconds(5),
-            bundling: {
-                minify: true,
-                sourceMap: false,
-            },
+			handler: "handler",
+			memorySize: 128,
+			timeout: Duration.seconds(5),
+			bundling: {
+				minify: true,
+				sourceMap: false,
+			},
+			environment: {
+				PRODUCTS_DDB: this.productsDdb.tableName
+			}
 		});
+
+		this.productsDdb.grantReadData(this.productsFetchHandler)
+
+		this.productsAdminHandler = new NodejsFunction(this, "ProductsAdminFunction", {
+			entry: "lambda/products/productsAdminFunction.ts",
+			functionName: "ProductsAdminFunction",
+			handler: "handler",
+			memorySize: 128,
+			timeout: Duration.seconds(5),
+			bundling: {
+				minify: true,
+				sourceMap: false,
+			},
+			environment: {
+				PRODUCTS_DDB: this.productsDdb.tableName
+			}
+		});
+
+		this.productsDdb.grantWriteData(this.productsAdminHandler)
 	}
 }
